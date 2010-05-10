721ccb313f004eeafd834e95a5496cf0db19d65e
snusnu <gamsnjaga@gmail.com>
2010-05-10 19:19:05 +0200

    Use dm-core/spec/setup in specs
    Currently there's 1 single spec failure when running
    either on mysql or postgres. It is related to the
    IPAddress custom type not being "large" enough to
    handle IPv6 addresses.
    
      http://pastie.org/954000.txt

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,43 +1,15 @@
-require 'rubygems'
-
-# TODO: autovalidation hooks are needed badly,
-#       otherwise plugin devs will have to abuse
-#       alising and load order even further and it kinda makes
-#       me sad -- MK
-
-# use local dm-core if running from a typical dev checkout.
-lib = File.join('..', '..', 'dm-core', 'lib')
-$LOAD_PATH.unshift(lib) if File.directory?(lib)
-
-# use local dm-validations if running from a typical dev checkout.
-lib = File.join('..', 'dm-validations', 'lib')
-$LOAD_PATH.unshift(lib) if File.directory?(lib)
-require 'dm-validations'
-
-# Support running specs with 'rake spec' and 'spec'
-$LOAD_PATH.unshift('lib') unless $LOAD_PATH.include?('lib')
+require 'dm-core/spec/setup'
+require 'dm-core/spec/lib/adapter_helpers'
 
 require 'dm-types'
+require 'dm-migrations'
+require 'dm-validations'
 
-def load_driver(name, default_uri)
-  return false if ENV['ADAPTER'] != name.to_s
+DataMapper::Spec.setup
 
-  begin
-    DataMapper.setup(name, ENV["#{name.to_s.upcase}_SPEC_URI"] || default_uri)
-    DataMapper::Repository.adapters[:default] =  DataMapper::Repository.adapters[name]
-    true
-  rescue LoadError => e
-    warn "Could not load do_#{name}: #{e}"
-    false
-  end
+Spec::Runner.configure do |config|
+  config.extend(DataMapper::Spec::Adapters::Helpers)
 end
-
-ENV['ADAPTER'] ||= 'sqlite3'
-
-HAS_SQLITE3  = load_driver(:sqlite3,  'sqlite3::memory:')
-HAS_MYSQL    = load_driver(:mysql,    'mysql://localhost/dm_core_test')
-HAS_POSTGRES = load_driver(:postgres, 'postgres://postgres@localhost/dm_core_test')
-
 
 DEPENDENCIES = {
   'bcrypt' => 'bcrypt-ruby',
