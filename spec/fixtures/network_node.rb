8c7131bf4933d95cd6e5309168e5fe9be10ce6c4
Dan Kubb <dan.kubb@gmail.com>
2010-09-07 14:06:38 -0700

    Changed usage of alias to alias_method

--- spec/fixtures/network_node.rb
+++ spec/fixtures/network_node.rb
@@ -21,8 +21,8 @@ module DataMapper
       # API
       #
 
-      alias uuid  node_uuid
-      alias uuid= node_uuid=
+      alias_method :uuid,  :node_uuid
+      alias_method :uuid=, :node_uuid=
 
       def runs_ipv6?
         self.ip_address.ipv6?
