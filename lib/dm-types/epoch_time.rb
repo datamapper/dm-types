39032c245cc4b3719d6c544ce2346f60431be4a4
Nico Rieck <nico.rieck@gmail.com>
2011-03-06 15:10:15 +0100

    Isolate core_ext/time.

--- lib/dm-types/epoch_time.rb
+++ lib/dm-types/epoch_time.rb
@@ -1,11 +1,5 @@
 require 'dm-core'
-
-begin
-  # provide Time#utc_time for DateTime#to_time in AS
-  require 'active_support/core_ext/time/calculations'
-rescue LoadError
-  # do nothing, extlib is being used and does not require this method
-end
+require 'dm-core/ext/date_time'
 
 module DataMapper
   class Property
@@ -21,8 +15,27 @@ module DataMapper
       def dump(value)
         case value
           when ::Integer, ::Time then value.to_i
-          when ::DateTime        then value.to_time.to_i
+          when ::DateTime        then datetime_to_time(value).to_i
         end
+      end
+
+    private
+      def datetime_to_time(datetime)
+        d = datetime
+        d.offset == 0 ? utc_time_with_fallback(d.year, d.month, d.day, d.hour, d.min, d.sec) : d
+      end
+
+      # Returns a new Time if requested year can be accommodated by Ruby's
+      # Time class (i.e., if year is within either 1970..2038 or 1902..2038,
+      # depending on system architecture); otherwise returns a DateTime
+      def utc_time_with_fallback(year, month = 1, day = 1, hour = 0, min = 0, sec = 0, usec = 0)
+        time = ::Time.utc(year, month, day, hour, min, sec, usec)
+
+        # This check is needed because Time.utc(y) returns a time object in the
+        # 2000s for 0 <= y <= 138.
+        time.year == year ? time : ::DateTime.civil(year, month, day, hour, min, sec, 0)
+      rescue
+        ::DateTime.civil(year, month, day, hour, min, sec, 0)
       end
     end # class EpochTime
   end # class Property
