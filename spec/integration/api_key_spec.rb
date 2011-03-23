8eaa011fe2b4800ba373e34df272ef86d8eaca08
Postmodern <postmodern.mod3@gmail.com>
2011-03-23 12:50:36 -0400

    Use subject/let in specs.

--- spec/integration/api_key_spec.rb
+++ spec/integration/api_key_spec.rb
@@ -5,23 +5,22 @@ try_spec do
 
   describe DataMapper::TypesFixtures::APIUser do
     supported_by :all do
-      before :all do
-        @resource = DataMapper::TypesFixtures::APIUser.new(:name => 'alice')
-        @original_api_key = @resource.api_key
-      end
+      subject { DataMapper::TypesFixtures::APIUser.new(:name => 'alice') }
+
+      let(:original_api_key) { subject.api_key }
 
       it "should have a default value" do
-        @original_api_key.should_not be_nil
+        original_api_key.should_not be_nil
       end
 
       it "should preserve the default value" do
-        @resource.api_key.should == @original_api_key
+        subject.api_key.should == original_api_key
       end
 
       it "should generate unique API Keys for each resource" do
         other_resource = DataMapper::TypesFixtures::APIUser.new(:name => 'eve')
 
-        other_resource.api_key.should_not == @original_api_key
+        other_resource.api_key.should_not == original_api_key
       end
     end
   end
