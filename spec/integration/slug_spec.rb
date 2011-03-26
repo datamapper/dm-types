9eb5f69437c648c79a87f231c8d9712a1c3e5049
Dan Kubb <dan.kubb@gmail.com>
2011-03-25 19:43:32 -0700

    Minor formatting fix in specs

--- spec/integration/slug_spec.rb
+++ spec/integration/slug_spec.rb
@@ -26,15 +26,15 @@ try_spec do
       end
 
       [
-       ['Iñtërnâtiônàlizætiøn',      'internationalizaetion' ],
-       ["This is Dan's Blog",        'this-is-dans-blog'],
-       ['This is My Site, and Blog', 'this-is-my-site-and-blog'],
-       ['Google searches for holy grail of Python performance', 'google-searches-for-holy-grail-of-python-performance'],
-       ['iPhone dev: Creating length-controlled data sources', 'iphone-dev-creating-length-controlled-data-sources'],
-       ["Review: Nintendo's New DSi -- A Quantum Leap Forward", 'review-nintendos-new-dsi-a-quantum-leap-forward'],
-       ["Arriva BraiVe, è l'auto-robot che si 'guida' da sola'", 'arriva-braive-e-lauto-robot-che-si-guida-da-sola'],
-       ["La ley antipiratería reduce un 33% el tráfico online en Suecia", 'la-ley-antipirateria-reduce-un-33-percent-el-trafico-online-en-suecia'],
-       ["L'Etat américain du Texas s'apprête à interdire Windows Vista", 'letat-americain-du-texas-sapprete-a-interdire-windows-vista']
+        [ 'Iñtërnâtiônàlizætiøn',                                           'internationalizaetion'                                                 ],
+        [ "This is Dan's Blog",                                             'this-is-dans-blog'                                                     ],
+        [ 'This is My Site, and Blog',                                      'this-is-my-site-and-blog'                                              ],
+        [ 'Google searches for holy grail of Python performance',           'google-searches-for-holy-grail-of-python-performance'                  ],
+        [ 'iPhone dev: Creating length-controlled data sources',            'iphone-dev-creating-length-controlled-data-sources'                    ],
+        [ "Review: Nintendo's New DSi -- A Quantum Leap Forward",           'review-nintendos-new-dsi-a-quantum-leap-forward'                       ],
+        [ "Arriva BraiVe, è l'auto-robot che si 'guida' da sola'",          'arriva-braive-e-lauto-robot-che-si-guida-da-sola'                      ],
+        [ "La ley antipiratería reduce un 33% el tráfico online en Suecia", 'la-ley-antipirateria-reduce-un-33-percent-el-trafico-online-en-suecia' ],
+        [ "L'Etat américain du Texas s'apprête à interdire Windows Vista",  'letat-americain-du-texas-sapprete-a-interdire-windows-vista'           ],
       ].each do |title, slug|
         describe "set with title '#{title}'" do
           before :all do
