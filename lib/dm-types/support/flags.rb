a5af997ad3a7b3e80e5fe66d74beb12db3faa797
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-05-17 12:45:49 -0700

    Move Flags support module into DataMapper::Types::Support.

--- lib/dm-types/support/flags.rb
+++ lib/dm-types/support/flags.rb
@@ -1,41 +1,44 @@
 module DataMapper
-  class Property
-    module Flags
-      def self.included(base)
-        base.class_eval <<-RUBY, __FILE__, __LINE__ + 1
-          extend DataMapper::Property::Flags::ClassMethods
+  module Types
+    module Support
+      module Flags
+        def self.included(base)
+          base.class_eval <<-RUBY, __FILE__, __LINE__ + 1
+            extend DataMapper::Types::Support::Flags::ClassMethods
 
-          accept_options :flags
-          attr_reader :flag_map
+            accept_options :flags
+            attr_reader :flag_map
 
-          class << self
-            attr_accessor :generated_classes
-          end
+            class << self
+              attr_accessor :generated_classes
+            end
 
-          self.generated_classes = {}
-        RUBY
-      end
-
-      def custom?
-        true
-      end
-
-      module ClassMethods
-        # TODO: document
-        # @api public
-        def [](*values)
-          if klass = generated_classes[values.flatten]
-            klass
-          else
-            klass = ::Class.new(self)
-            klass.flags(values)
-
-            generated_classes[values.flatten] = klass
-
-            klass
-          end
+            self.generated_classes = {}
+          RUBY
         end
-      end
-    end
-  end
-end
+
+        def custom?
+          true
+        end
+
+        module ClassMethods
+          # TODO: document
+          # @api public
+          def [](*values)
+            if klass = generated_classes[values.flatten]
+              klass
+            else
+              klass = ::Class.new(self)
+              klass.flags(values)
+
+              generated_classes[values.flatten] = klass
+
+              klass
+            end
+          end
+        end # module ClassMethods
+
+      end # module Flags
+    end # module Support
+  end # module Types
+end # module DataMapper
