d87497939a40c63404ea0569420399578987ae02
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-08-16 13:05:59 -0700

    In DirtyMinder, test #nil? instead of #frozen?.
    Also outdent the private keyword, because outdented method visibility declarations are the bees knees.

--- lib/dm-types/support/dirty_minder.rb
+++ lib/dm-types/support/dirty_minder.rb
@@ -137,17 +137,17 @@ module DataMapper
 
       # This catches any direct assignment, allowing us to hook the Hash or Array.
       def set(resource, value)
-        hook_value(resource, value) unless value.frozen? || value.kind_of?(Hooker)
+        hook_value(resource, value) unless value.nil? || value.kind_of?(Hooker)
         super
       end
 
       # This gets called when Resource#reload is called (instead of #set).
       def set!(resource, value)
-        hook_value(resource, value) unless value.frozen? || value.kind_of?(Hooker)
+        hook_value(resource, value) unless value.nil? || value.kind_of?(Hooker)
         super
       end
 
-      private
+    private
 
       def hook_value(resource, value)
         return if value.kind_of? Hooker
